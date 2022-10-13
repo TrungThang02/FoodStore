30906e35d7ef48a94076bc4d7588f7a9ec5a6e5b
trantrungthang <80338828+trantrungthang@users.noreply.github.com>
2022-10-13 14:59:10 +0700

    upload

---
+++ FoodStore/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FoodStore.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+       public ActionResult NavbarPartial()
+        {
+            return PartialView();
+        }
+        public ActionResult NavigationPartial()
+        {
+            return PartialView();
+        }
+
+        public ActionResult BannerPartial()
+        {
+            return PartialView();
+        }
+
+
+        public ActionResult TopSellingPartial()
+        {
+            return PartialView();
+        }
+    }
+}
